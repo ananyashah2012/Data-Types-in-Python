dbbaf4ecf38f4dee0ecf362cc2e121e6e3ee7866
ananyashah2012 <ananyashah@thehumanistacademy.org>
2026-03-19 17:13:43 -0500

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,8 @@
+a=4
+print("The data type of a is", type(a))
+b=2.5
+print("The data type of b is", type(b))
+c=True
+print("The data type of c is", type(c))
+d="My name is Ananya"
+print("The data type of d is", type (d))
