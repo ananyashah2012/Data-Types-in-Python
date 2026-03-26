dfeb5d08b6ad22954ed66b7e30f08b3506010b85
ananyashah2012 <ananyashah@thehumanistacademy.org>
2026-03-26 17:01:53 -0500

    Add files via upload

--- Activity1.py
+++ Activity1.py
@@ -1,8 +1,11 @@
-a=4
-print("The data type of a is", type(a))
-b=2.5
-print("The data type of b is", type(b))
-c=True
-print("The data type of c is", type(c))
-d="My name is Ananya"
-print("The data type of d is", type (d))
+tree1 = 98
+tree2 = 94
+tree3 = 56
+tree4 = 24
+tree5 = 102
+
+sum = tree1+tree2+tree3+tree4+tree5
+print("The sum of all 5 trees is: ", sum)
+
+average = sum/5
+print("The average of all 5 trees is: ", average)
