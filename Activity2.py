4a5077388ece6edc2a734be6e8caf7b17e8444b9
ananyashah2012 <ananyashah@thehumanistacademy.org>
2026-03-26 17:18:12 -0500

    Add files via upload

--- Activity2.py
+++ Activity2.py
@@ -1,8 +1,9 @@
-a=5
-b=2.5
-c=True
-f="13"
-a=float(a)
-print("The data type of a is", type(a))
-b=int(b)
-print("The data type of b is", type(b))
+Amount = int(input("Please Enter Amount for Withdraw :"))
+
+note_1 = Amount//100
+note_2 = (Amount%100)//50
+note_3 = ((Amount%100)%50)//10
+
+print( "The number of 100 dollar notes", note_1)
+print( "The number of 50 dollar notes", note_2)
+print( "The number of 10 dollar notes", note_3)
