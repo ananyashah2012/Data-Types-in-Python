4f86d4fab32a3ebd1f875cd711af438942e0382c
ananyashah2012 <ananyashah@thehumanistacademy.org>
2026-03-26 17:27:54 -0500

    Add files via upload

--- Activity3.py
+++ Activity3.py
@@ -1,6 +1,11 @@
-a="I love to travel"
-print(a.upper())
-print(a.lower())
-print(a[2:6])
-name=input("enter your name")
-print("Your name is", name)
+print("Enter Marks Obtained in 4 Subjects")
+
+Math = int(input("enter Math"))
+English = int(input("enter English"))
+Science = int(input("enter Science"))
+History = int(input("enter History"))
+
+Sum = Math+English+Science+History
+Percentage = (Sum/400)*100
+
+print("The Percentage is", Percentage)
