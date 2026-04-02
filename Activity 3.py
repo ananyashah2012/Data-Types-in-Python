80328a741d8a8c721aa1f26eb0cbccfc10c0b354
ananyashah2012 <ananyashah@thehumanistacademy.org>
2026-04-02 17:23:41 -0500

    Add files via upload

---
+++ Activity 3.py
@@ -0,0 +1,9 @@
+x = int(input("enter a number : "))
+if (x < 25):
+    print("X is less than 25")
+    print("X is in if Block")
+    
+else:
+    print("X is greater than 25")
+    print("X is in else Block")
+print("X isn't in if Block or else Block")
